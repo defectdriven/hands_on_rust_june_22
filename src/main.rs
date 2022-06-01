c3a02d1fc7b87ee378b92184f973a608989978b1
Mike Ask <mikeask@Mask-M1.local>
2022-06-01 08:56:46 -0500

    Fixing issue with camera in map.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+#![warn(clippy::pedantic)]
+
 mod map;
-mod player;
 mod map_builder;
+mod player;
 mod camera;
 
 mod prelude {
@@ -27,8 +29,8 @@ impl State {
     fn new() -> Self {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
-        Self { 
-            map: map_builder.map,
+        Self {
+            map : map_builder.map,
             player: Player::new(map_builder.player_start),
             camera: Camera::new(map_builder.player_start)
         }
@@ -41,7 +43,6 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-
         self.player.update(ctx, &self.map, &mut self.camera);
         self.map.render(ctx, &self.camera);
         self.player.render(ctx, &self.camera);
@@ -49,15 +50,17 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()// (1)
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
-        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
-        .with_tile_dimensions(32, 32)
-        .with_resource_path("resources/")
-        .with_font("dungeonfont.png", 32, 32)
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) // (2)
+        .with_tile_dimensions(32, 32) // (3)
+        .with_resource_path("resources/") // (4)
+        .with_font("dungeonfont.png", 32, 32) // (5)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (6)
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, 
+            "dungeonfont.png") // (7)
         .build()?;
+
     main_loop(context, State::new())
 }
