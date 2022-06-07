83d5f62a58ca16b52a3381946047a7bebd17ba21
Mike Ask <mikeask@Mask-M1.local>
2022-06-07 13:48:34 -0500

    Using cellular automata algorithm room architect

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,9 @@ impl State {
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
         spawn_amulet(&mut ecs, map_builder.amulet_start);
+        map_builder.monster_spawns
+            .iter()
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, *pos));
         map_builder
             .rooms
             .iter()
@@ -88,6 +91,9 @@ impl State {
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut self.ecs, map_builder.player_start);
         spawn_amulet(&mut self.ecs, map_builder.amulet_start);
+        map_builder.monster_spawns
+            .iter()
+            .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, *pos));
         map_builder
             .rooms
             .iter()
--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,47 +1,47 @@
 use crate::prelude::*;
+mod empty;
+use empty::EmptyArchitect;
+mod rooms;
+use rooms::RoomsArchitect;
+mod automata;
+use automata::CellularAutomataArchitect;
+trait MapArchitect {
+    fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
+}
 const NUM_ROOMS: usize = 20;
+const UNREACHABLE: &f32 = &f32::MAX;
 
 pub struct MapBuilder {
     pub map: Map,
     pub rooms: Vec<Rect>,
+    pub monster_spawns : Vec<Point>,
     pub player_start: Point,
     pub amulet_start: Point,
 }
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        const UNREACHABLE: &f32 = &f32::MAX;
-        let mut mb = MapBuilder {
-            map: Map::new(),
-            rooms: Vec::new(),
-            player_start: Point::zero(),
-            amulet_start: Point::zero(),
-        };
-        mb.fill(TileType::Wall);
-        mb.build_random_rooms(rng);
-        mb.build_corridors(rng);
-        mb.player_start = mb.rooms[0].center();
+        let mut architect = CellularAutomataArchitect{};
+        architect.new(rng)
+    }
 
+    fn find_most_distant(&self) -> Point {
         let dijkstra_map = DijkstraMap::new(
             SCREEN_WIDTH,
             SCREEN_HEIGHT,
-            &vec![mb.map.point2d_to_index(mb.player_start)],
-            &mb.map,
-            1024.0,
+            &vec![self.map.point2d_to_index(self.player_start)],
+            &self.map,
+            1024.0
         );
 
-        mb.amulet_start = mb.map.index_to_point2d(
-            dijkstra_map
-                .map
+        self.map.index_to_point2d(
+            dijkstra_map.map
                 .iter()
                 .enumerate()
                 .filter(|(_, dist)| *dist < UNREACHABLE)
-                .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
-                .unwrap()
-                .0,
-        );
-
-        mb
+                .max_by(|a,b| a.1.partial_cmp(b.1).unwrap())
+                .unwrap().0
+        )
     }
 
     fn fill(&mut self, tile: TileType) {
@@ -49,24 +49,20 @@ impl MapBuilder {
     }
 
     fn build_random_rooms(&mut self, rng: &mut RandomNumberGenerator) {
-        // (2)
         while self.rooms.len() < NUM_ROOMS {
-            // (3)
             let room = Rect::with_size(
-                // (4)
                 rng.range(1, SCREEN_WIDTH - 10),
                 rng.range(1, SCREEN_HEIGHT - 10),
                 rng.range(2, 10),
                 rng.range(2, 10),
             );
-            let mut overlap = false; // (5)
+            let mut overlap = false;
             for r in self.rooms.iter() {
                 if r.intersect(&room) {
                     overlap = true;
                 }
             }
             if !overlap {
-                // (6)
                 room.for_each(|p| {
                     if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
                         let idx = idx(p.x, p.y);
@@ -99,15 +95,13 @@ impl MapBuilder {
 
     fn build_corridors(&mut self, rng: &mut RandomNumberGenerator) {
         let mut rooms = self.rooms.clone();
-        rooms.sort_by(|a, b| a.center().x.cmp(&b.center().x)); // (7)
+        rooms.sort_by(|a, b| a.center().x.cmp(&b.center().x)); 
 
         for (i, room) in rooms.iter().enumerate().skip(1) {
-            // (8)
-            let prev = rooms[i - 1].center(); // (9)
+            let prev = rooms[i - 1].center();
             let new = room.center();
 
             if rng.range(0, 2) == 1 {
-                // (10)
                 self.apply_horizontal_tunnel(prev.x, new.x, prev.y);
                 self.apply_vertical_tunnel(prev.y, new.y, new.x);
             } else {
@@ -115,5 +109,32 @@ impl MapBuilder {
                 self.apply_horizontal_tunnel(prev.x, new.x, new.y);
             }
         }
+    }
+
+    fn spawn_monsters (
+        &self,
+        start: &Point,
+        rng: &mut RandomNumberGenerator
+    ) -> Vec<Point> {
+        const NUM_MONSTERS : usize = 50;
+        let mut spawnable_tiles : Vec<Point> = self.map.tiles
+            .iter()
+            .enumerate()
+            .filter(|(idx, t)|
+                **t == TileType::Floor &&
+                    DistanceAlg::Pythagoras.distance2d(
+                        *start,
+                        self.map.index_to_point2d(*idx)
+                    ) > 10.0
+                )
+            .map(|(idx, _)| self.map.index_to_point2d(idx))
+            .collect();
+        let mut spawns = Vec::new();
+        for _ in 0 .. NUM_MONSTERS {
+            let target_index = rng.random_slice_index(&spawnable_tiles).unwrap();
+            spawns.push(spawnable_tiles[target_index].clone());
+            spawnable_tiles.remove(target_index);
+        }
+        spawns
     }
 }
